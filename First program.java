a751a2961b94c9bcdea58dfe7dd9a6ba30ffcd37
Santosh Hembram <santoshhembramis17489@gmail.com>
2020-07-26 21:51:03 +0530

    Simple Java program to display output.

---
+++ First program.java
@@ -0,0 +1,10 @@
+/*My first java Program*/
+
+class FirstProgram //OOP and without class we cannot write a single line in java
+{
+		public static void main(String args[])
+		{
+				System.out.println("Hembram");
+				System.out.println("Hembram");
+		}
+}
