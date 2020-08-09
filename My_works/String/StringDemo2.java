edb1fe58c61fa307bbbab5ff7db41f991c4abcfb
Santosh Hembram <santoshhembramis17489@gmail.com>
2020-08-09 14:23:15 +0530

    find length of a string

---
+++ My_works/String/StringDemo2.java
@@ -0,0 +1,12 @@
+//Reading the string and finding the length of the string
+	
+	class StringDemo2
+	{
+		public static void main(String args[])
+		{
+			String str = new String("Hello");
+			System.out.println(str);
+		
+			System.out.println(str.length());
+		}
+	}
