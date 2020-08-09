1eed5f0dbe0086d5f2bae68ec02b80a52a131608
Santosh Hembram <santoshhembramis17489@gmail.com>
2020-08-09 14:22:05 +0530

    Concatenate the string

---
+++ My_works/String/StringDemo1.java
@@ -0,0 +1,12 @@
+//Concatenate the string
+	
+	class StringDemo1
+	{
+		public static void main(String args[])
+		{
+			String str = new String("Hello");
+			System.out.println(str);
+			str = str.concat(" Welcome");
+			System.out.println(str);
+		}
+	}
