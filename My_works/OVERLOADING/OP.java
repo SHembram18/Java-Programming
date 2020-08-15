eaebebc499fe8b944b47503a14b1d5e4a26b5aee
Santosh Hembram <santoshhembramis17489@gmail.com>
2020-08-15 11:29:46 +0530

    Operator overlaoding

---
+++ My_works/OVERLOADING/OP.java
@@ -0,0 +1,16 @@
+//Operator overloading
+
+	class OP
+	{
+		void add()
+		{
+			int a=10,b=30;
+			System.out.println("a+b= "+(a+b));
+		}
+		public static void main(String arg[])
+		{
+			OP obj = new OP();
+			obj.add();
+		}
+	}
+	
