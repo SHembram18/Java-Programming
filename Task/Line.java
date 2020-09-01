022abec0fe6981dba92e11d764d093e7ba931712
Santosh Hembram <santoshhembramis17489@gmail.com>
2020-09-01 09:34:45 +0530

    Print a string consisting of 5 words using command

---
+++ Task/Line.java
@@ -0,0 +1,20 @@
+class Line
+{
+	public static void main(String args[])
+	{
+		
+		
+			 System.out.print(args[0]);
+			 System.out.print(" ");
+			 System.out.print(args[1]);
+			 System.out.print(" ");
+			 System.out.print(args[2]);
+			 System.out.print(" ");
+			 System.out.print(args[3]);
+			 System.out.print(" ");
+			 System.out.print(args[4]);
+			
+		
+		
+	}
+}
