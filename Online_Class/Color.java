c7b1e73fed53709f61626be2c0f6b96785465e69
Santosh Hembram <santoshhembramis17489@gmail.com>
2020-09-07 08:12:58 +0530

    ENUM as Class DEMONSTRATION PROGRAM IN JAVA

---
+++ Online_Class/Color.java
@@ -0,0 +1,14 @@
+/*
+	ENUM as Class DEMONSTRATION PROGRAM IN JAVA
+*/
+
+	enum Color{
+
+		RED, GREEN, BLUE, BLACK, YELLOW ;
+
+		public static void main(String args[]){
+			Color c1 = Color.GREEN;
+
+			System.out.println("Color is : " + c1);
+		} 
+	}
