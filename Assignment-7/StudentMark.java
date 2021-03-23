af2b234a32429f9cc51529647ed5dd46e3774606
Santosh Hembram <santoshhembramis17489@gmail.com>
2021-03-23 17:02:13 +0530

    Update StudentMark.java

--- Assignment-7/StudentMark.java
+++ Assignment-7/StudentMark.java
@@ -23,33 +23,40 @@ class A implements Marks {
 
  		secured_marks = math + gsc + eng;
  		System.out.println("Your score = "+secured_marks);
-
  	}
  	public float getPercentage(){
 
  		float per = (float)(secured_marks * 100) / total_marks;
+ 		return per;
+ 	}
+}
+class B implements Marks {
 
+	int total_marks = 400 , secured_marks;
+
+	B ( int math, int gsc, int eng, int practical) {
+
+		secured_marks = math + gsc + eng + practical;
+		System.out.println("Your score = "+secured_marks);
+
+	}
+	public float getPercentage(){
+
+ 		float per = (float)(secured_marks * 100) / total_marks;
  		return per;
  	}
 
 }	
 
-/* class B implements Marks {
-
- 	B(float math, float gsc, float eng, float hindi) {
-
- 	}
-
- }	*/    
-
- class StudentMark {
+	    
+class StudentMark {
 
  	public static void main(String[] args) {
  		int math,gsc,eng;
 
  		Scanner sc = new Scanner(System.in);
  		
- 		System.out.println("----- Enter the mark for student A ------");
+ 		System.out.println("\n----- Enter the mark for student A ------\n");
  		
  			System.out.print("Mark obtained in Math out of 100 : ");
  			math = sc.nextInt();
@@ -75,14 +82,64 @@ class A implements Marks {
  				eng = sc.nextInt();
  			}
 
+
+
+
+ 			System.out.println("\n----- Enter the mark for student B ------\n");
+ 		
+ 			System.out.print("Mark obtained in Math out of 100 : ");
+ 			int mth = sc.nextInt();
+ 			while(mth>100 || mth<0) {
+ 				System.out.println("-----TRY AGAIN----");
+ 				System.out.print("Mark obtained in Math out of 100 : ");
+ 				mth = sc.nextInt();
+ 			}
+
+ 			System.out.print("Mark obtained in Science out of 100 : ");
+ 			int sci = sc.nextInt();
+ 			while(sci>100 || sci<0) {
+ 				System.out.println("-----TRY AGAIN----");
+ 				System.out.print("Mark obtained in Science out of 100 : ");
+ 				sci = sc.nextInt();
+ 			}
+
+ 			System.out.print("Mark obtained in English out of 100 : ");
+ 			int engl = sc.nextInt();
+ 			while(engl>100 || engl<0) {
+ 				System.out.println("-----TRY AGAIN----");
+ 				System.out.print("Mark obtained in English out of 100 : ");
+ 				engl = sc.nextInt();
+ 			}
+
+ 			System.out.print("Mark obtained in Practical out of 100 : ");
+ 			int pract = sc.nextInt();
+ 			while(pract>100 || pract<0) {
+ 				System.out.println("-----TRY AGAIN----");
+ 				System.out.print("Mark obtained in Practical out of 100 : ");
+ 				pract = sc.nextInt();
+ 			}
+
+
+
+ 			System.out.println("\n========= DISPLAYING THE RESULT OF A =============");
+
  			Marks obj;
  			obj = new A(math,gsc,eng);
  			float per = obj.getPercentage();
  			System.out.printf("percentage = %.2f",per,"%");
 
- 		
+ 			System.out.println("\n========= DISPLAYING THE RESULT OF B =============");
+
+ 			obj = new B(mth,sci,engl,pract);
+ 			float per2 = obj.getPercentage();
+ 			System.out.printf("percentage = %.2f",per2,"%");
+
+
+
 
  	}
- }
+}
+
+	
 
 	
